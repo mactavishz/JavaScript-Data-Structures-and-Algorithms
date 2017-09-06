a7913c25357910fc04ca0664b504b230997806db
mactavish <maczhanchao@yahoo.com>
2017-09-06 23:55:21 +0800

    add Stack code

---
+++ Stack/Stack.js
@@ -0,0 +1,26 @@
+module.exports = class Stack {
+  constructor() {
+    this.items = []
+  }
+  push(ele) {
+    this.items.push(ele)
+  }
+  pop() {
+    return this.items.pop()
+  }
+  top() {
+    return this.items[this.items.length - 1]
+  }
+  isEmpty() {
+    return this.items.length === 0
+  }
+  size() {
+    return this.items.length
+  }
+  destroy() {
+    this.items = []
+  }
+  print() {
+    console.log(this.items)
+  }
+}
