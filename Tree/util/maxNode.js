7f39be64ed8818c54847f7f8cd37c7a89d3c1c44
詹潮 <maczhancaho@yahoo.com>
2017-09-19 15:42:04 +0800

    remove file

--- Tree/util/maxNode.js
+++
@@ -1,9 +0,0 @@
-module.exports = function maxNode(node) {
-  if(node) {
-    while(node && node.right) {
-      node = node.right
-    }
-    return node.key
-  }
-  return null
-}
--- Tree/util/minNode.js
+++
@@ -1,9 +0,0 @@
-module.exports = function minNode(node) {
-  if(node) {
-    while(node && node.left) {
-      node = node.left
-    }
-    return node.key
-  }
-  return null
-}
