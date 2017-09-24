c2d8de5500dae5031f556d31fbb143867c5cb588
Mactavish <maczhanchao@yahoo.com>
2017-09-24 21:42:59 +0800

    add quick sort

---
+++ Sort&Search/quickSort.js
@@ -0,0 +1,42 @@
+const swap = require('./util/swap')
+
+// Time complexity O(nlog(n))
+// best-case: O(nlog(n))
+// worst-case: O(n^2)
+// average: O(nlog(n))
+//
+// implement insertSort without using swap function
+function quickSort(array, left = 0, right = arr.length - 1) {
+  var index
+  if(array.length > 1) {
+    index = partition(array, left, right)
+
+    if(left < index - 1) {
+      quickSort(array, left, index - 1)
+    }
+
+    if(left < right) {
+      quickSort(array, index, right)
+    }
+  }
+}
+
+function partition(array, left, right) {
+  var pivot = array[Math.floor((left + right) / 2)]
+
+  while(left <= right) {
+    while(array[left] < pivot) {
+      left++
+    }
+    while(array[right] > pivot) {
+      right--
+    }
+
+    if(left <= right) {
+      swap(array, left, right)
+      left++
+      right--
+    }
+  }
+  return left
+}
